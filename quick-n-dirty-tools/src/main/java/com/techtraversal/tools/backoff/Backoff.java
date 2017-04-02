4dbb1a6de1c1f62bea7c0c7ff8d49df21b344e68
sriv-priyank <sriv-priyank@users.noreply.github.com>
2017-04-02 23:55:49 +0530

    exponential backoff implementation; pending time-elapsed check

--- quick-n-dirty-tools/src/main/java/com/techtraversal/tools/backoff/Backoff.java
+++ quick-n-dirty-tools/src/main/java/com/techtraversal/tools/backoff/Backoff.java
@@ -1,12 +1,24 @@
 package com.techtraversal.tools.backoff;
 
 
+import com.techtraversal.tools.tmp.BackOff;
+
+import java.io.IOException;
+
 public interface Backoff {
 
     static final long STOP = -1L;
 
-    /** Reset to initial state. */
     void reset();
 
     long nextInterval();
+
+    BackOff NONE = new BackOff() {
+
+        public void reset() {}
+
+        public long nextBackOffMillis() {
+            return 0;
+        }
+    };
 }
--- quick-n-dirty-tools/src/main/java/com/techtraversal/tools/backoff/ExponentialBackoff.java
+++ quick-n-dirty-tools/src/main/java/com/techtraversal/tools/backoff/ExponentialBackoff.java
@@ -3,13 +3,69 @@ package com.techtraversal.tools.backoff;
 
 public final class ExponentialBackoff implements Backoff {
 
+    // all intervals are in milliseconds
+
+    private static final long   DEFAULT_INITIAL_INTERVAL    = 500L;
+    private static final double DEFAULT_FACTOR              = 1.5d;
+    private static final long   DEFAULT_MAX_INTERVAL        = 60000l;
+
+    private long initialInterval, maxInterval, currentInterval;
+    private double factor;
+
+
+    private ExponentialBackoff() {
+        this.initialInterval = DEFAULT_INITIAL_INTERVAL;
+        this.factor = DEFAULT_FACTOR;
+        this.maxInterval = DEFAULT_MAX_INTERVAL;
+    }
+
     @Override
     public final void reset() {
-
+        this.currentInterval = this.initialInterval;
     }
 
     @Override
     public final long nextInterval() {
-        return 0;
+        if (this.currentInterval >= this.maxInterval / this.factor)
+            this.currentInterval = this.maxInterval;
+        else
+            this.currentInterval *= this.factor;
+
+        return this.currentInterval;
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    public static ExponentialBackoff instance() {
+        return new ExponentialBackoff();
+    }
+
+    public ExponentialBackoff initialInterval(final long initialInterval) {
+        this.initialInterval = initialInterval;
+        return this;
+    }
+
+    public ExponentialBackoff factor(final double factor) {
+        this.factor = factor;
+        return this;
+    }
+
+    public ExponentialBackoff maxInterval(final long maxInterval) {
+        this.maxInterval = maxInterval;
+        return this;
+    }
+
+    /////////////////////////////////////////////////////////////////
+
+    public long getInitialInterval() {
+        return this.initialInterval;
+    }
+
+    public double getFactor() {
+        return this.factor;
+    }
+
+    public long getMaxInterval() {
+        return this.maxInterval;
     }
 }
